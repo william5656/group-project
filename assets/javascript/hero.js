4ddb48a90c777e45b1f84bce46582840ea3783fb
Chestirito <19494641+Chestirito@users.noreply.github.com>
2018-07-12 21:31:32 -0400

    user click on search button, redirect to hero.html, display story and image

---
+++ assets/javascript/hero.js
@@ -0,0 +1,34 @@
+$(function(){
+
+    var hero = {
+        name : "",
+        deck : "",
+        description : "",
+        realName : "",
+        origin : "",
+        imageUrl : ""
+    }
+    
+    var query = localStorage.getItem("query");
+    //console.log(query);
+    var key = "390f1045415ee2e2bbb6b090a5a6cc8457d2f4f0";
+    //var test = "https://cors-anywhere.herokuapp.com/https://comicvine.gamespot.com/api/characters/?api_key="+ key +"&filter=name%3Adeadpool&format=JSON";
+    var queryURL = "https://cors-anywhere.herokuapp.com/https://comicvine.gamespot.com/api/characters/?api_key=" + key + "&filter=name:" + query + "&limit=1&format=JSON";
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        console.log(response);
+        //console.log(response.results[0].deck);
+        hero.name = response.results[0].name;
+        hero.deck = response.results[0].deck;
+        hero.description = response.results[0].description;
+        hero.realName = response.results[0].real_name;
+        hero.origin = response.results[0].origin;
+        hero.imageUrl = response.results[0].image.original_url;
+        $(".story").text(hero.deck);
+        $(".mainImage").attr("src", hero.imageUrl);
+    });
+});
+
+
--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,1 +1,12 @@
+$(function(){
+    $(".searchBtn").on("click", function(){
+        var getInput = $(".searchTerm").val();
+        //console.log(getInput);
+        localStorage.setItem("query",getInput);
+    });
+    //localStorage.setItem("query",getInput);
+
+    
+});
+
 
