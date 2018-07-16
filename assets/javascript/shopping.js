3fe31aee263e26b4e921a249083686ff1ac305c0
DFloor Danny <37215615+dan-homoncik@users.noreply.github.com>
2018-07-15 22:45:43 -0400

    working JSON parsed response for ebay API

--- assets/javascript/shopping.js
+++ assets/javascript/shopping.js
@@ -2,13 +2,20 @@ $(function(){
 
     var shoppingQuery = localStorage.getItem("query");
 
-    var shoppingQueryURL = "https://cors-anywhere.herokuapp.com/http://open.api.ebay.com/shopping?&callname=FindItems&appID=DanHomon-s-PRD-98bb06b1d-de4b9aaa&QueryKeywords=" + shoppingQuery
+    // var shoppingQueryURL = "http://open.api.ebay.com/shopping?callname=FindProducts&responseencoding=JSON&appid=DanHomon-s-PRD-98bb06b1d-de4b9aaa&siteid=0&version=967&QueryKeywords=" + shoppingQuery + "&AvailableItemsOnly=true&MaxEntries=5"
+
+    var shoppingQueryURL = "https://cors-anywhere.herokuapp.com/http://open.api.ebay.com/shopping?callname=FindProducts&responseencoding=JSON&appid=DanHomon-s-PRD-98bb06b1d-de4b9aaa&siteid=0&version=967&QueryKeywords=deadpool&AvailableItemsOnly=true&MaxEntries=3"
+
 
     $.ajax({
         url: shoppingQueryURL,
         method: "GET"
     }).then(function(response) {
-        console.log(response);
+        var json = JSON.parse(response);
+        console.log(json);
+        console.log(json.Product[0].DetailsURL);
+        console.log(json.Product[0].Title);
+
     });
 
 });
