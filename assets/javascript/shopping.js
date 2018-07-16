9a6ad1056c52c74d5e53645dbc94a0a246658bf6
DFloor Danny <37215615+dan-homoncik@users.noreply.github.com>
2018-07-15 23:35:33 -0400

    updated hero page for shopping links and displayed shopping links

--- assets/javascript/shopping.js
+++ assets/javascript/shopping.js
@@ -2,13 +2,32 @@ $(function(){
 
     var shoppingQuery = localStorage.getItem("query");
 
-    var shoppingQueryURL = "https://cors-anywhere.herokuapp.com/http://open.api.ebay.com/shopping?&callname=FindItems&appID=DanHomon-s-PRD-98bb06b1d-de4b9aaa&QueryKeywords=" + shoppingQuery
+    // var shoppingQueryURL = "http://open.api.ebay.com/shopping?callname=FindProducts&responseencoding=JSON&appid=DanHomon-s-PRD-98bb06b1d-de4b9aaa&siteid=0&version=967&QueryKeywords=" + shoppingQuery + "&AvailableItemsOnly=true&MaxEntries=5"
+
+    var shoppingQueryURL = "https://cors-anywhere.herokuapp.com/http://open.api.ebay.com/shopping?callname=FindProducts&responseencoding=JSON&appid=DanHomon-s-PRD-98bb06b1d-de4b9aaa&siteid=0&version=967&QueryKeywords=deadpool&AvailableItemsOnly=true&MaxEntries=3"
+
 
     $.ajax({
         url: shoppingQueryURL,
         method: "GET"
     }).then(function(response) {
-        console.log(response);
+        var json = JSON.parse(response);
+        console.log(json);
+        console.log(json.Product[0].DetailsURL);
+        console.log(json.Product[0].Title);
+
+
+
+        for(var i = 0; i < json.Product.length; i++) {
+            var newDiv = $("<a>");
+            newDiv.addClass("shoppingLinks");
+            newDiv.attr("href", json.Product[i].DetailsURL);
+            newDiv.text(json.Product[i].Title);
+            $(".ebayLinks").append(newDiv);
+            $(".ebayLinks").append("<br><br>");
+
+        }
+
     });
 
 });
