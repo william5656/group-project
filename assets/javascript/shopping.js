6bce7347683f4f9cf454c2dbcb3268917efe17bb
Chestirito <19494641+Chestirito@users.noreply.github.com>
2018-07-16 13:02:48 -0400

    shopping.js grab query from local storage

--- assets/javascript/shopping.js
+++ assets/javascript/shopping.js
@@ -4,7 +4,7 @@ $(function(){
 
     // var shoppingQueryURL = "http://open.api.ebay.com/shopping?callname=FindProducts&responseencoding=JSON&appid=DanHomon-s-PRD-98bb06b1d-de4b9aaa&siteid=0&version=967&QueryKeywords=" + shoppingQuery + "&AvailableItemsOnly=true&MaxEntries=5"
 
-    var shoppingQueryURL = "https://cors-anywhere.herokuapp.com/http://open.api.ebay.com/shopping?callname=FindProducts&responseencoding=JSON&appid=DanHomon-s-PRD-98bb06b1d-de4b9aaa&siteid=0&version=967&QueryKeywords=deadpool&AvailableItemsOnly=true&MaxEntries=3"
+    var shoppingQueryURL = "https://cors-anywhere.herokuapp.com/http://open.api.ebay.com/shopping?callname=FindProducts&responseencoding=JSON&appid=DanHomon-s-PRD-98bb06b1d-de4b9aaa&siteid=0&version=967&QueryKeywords=" + shoppingQuery + "&AvailableItemsOnly=true&MaxEntries=3"
 
 
     $.ajax({
