f9b55c193eb6772a8ba546d00056fd7c20ef0de6
Chestirito <19494641+Chestirito@users.noreply.github.com>
2018-07-17 03:24:57 -0400

    changed ebay api from shopping to finding

--- assets/javascript/shopping.js
+++ assets/javascript/shopping.js
@@ -4,8 +4,9 @@ $(function(){
 
     // var shoppingQueryURL = "http://open.api.ebay.com/shopping?callname=FindProducts&responseencoding=JSON&appid=DanHomon-s-PRD-98bb06b1d-de4b9aaa&siteid=0&version=967&QueryKeywords=" + shoppingQuery + "&AvailableItemsOnly=true&MaxEntries=5"
 
-    var shoppingQueryURL = "https://cors-anywhere.herokuapp.com/http://open.api.ebay.com/shopping?callname=FindProducts&responseencoding=JSON&appid=DanHomon-s-PRD-98bb06b1d-de4b9aaa&siteid=0&version=967&QueryKeywords=" + shoppingQuery + "&AvailableItemsOnly=true&MaxEntries=3"
-
+    shoppingQueryURL = "https://cors-anywhere.herokuapp.com/https://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.0.0&SECURITY-APPNAME=DanHomon-s-PRD-98bb06b1d-de4b9aaa&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&keywords=" 
+    + shoppingQuery
+    + "&itemFilter.name=TopRatedSellerOnly&itemFilter.value=true" ;
 
     $.ajax({
         url: shoppingQueryURL,
@@ -18,11 +19,11 @@ $(function(){
 
 
 
-        for(var i = 0; i < json.Product.length; i++) {
+        for(var i = 0; i < 6; i++) {
             var newDiv = $("<a>");
             newDiv.addClass("shoppingLinks");
-            newDiv.attr("href", json.Product[i].DetailsURL);
-            newDiv.text(json.Product[i].Title);
+            newDiv.attr("href", json.findItemsByKeywordsResponse[0].searchResult[0].item[i].viewItemURL[0]);
+            newDiv.text(json.findItemsByKeywordsResponse[0].searchResult[0].item[i].title[0]);
             $(".ebayLinks").append(newDiv);
             $(".ebayLinks").append("<br><br>");
 
@@ -30,7 +31,7 @@ $(function(){
 
     });
 
-    function ajaxCall(item){
+    function toyAjax(item){
         //var shoppingQuery = localStorage.getItem("query");
 
         // var shoppingQueryURL = "http://open.api.ebay.com/shopping?callname=FindProducts&responseencoding=JSON&appid=DanHomon-s-PRD-98bb06b1d-de4b9aaa&siteid=0&version=967&QueryKeywords=" + shoppingQuery + "&AvailableItemsOnly=true&MaxEntries=5"
@@ -71,5 +72,5 @@ $(function(){
         });
     }
 
-    ajaxCall("toy");
+    toyAjax("toy");
 });
